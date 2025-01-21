0e850ca0283f26f8ef48f52ab75054476326b093
Srivignesh Kavle <ramnathkavle@gmail.com>
2025-01-21 16:45:29 -0500

    Adding week2 content

---
+++ week2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week2 | Variables</title>
+</head>
+<body>
+    <?php
+        $name  = "Srivignesh";
+    ?>
+    Hi <?php echo $name?>!
+    <?php echo "Hey," .$name. "!"?>
+</body>
+</html>
