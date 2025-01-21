e4d0a583169183f96e23d2052be0d1fffb321ac3
Srivignesh Kavle <ramnathkavle@gmail.com>
2025-01-21 17:55:52 -0500

    Updated inclass activity

--- week2/index.php
+++ week2/index.php
@@ -1,15 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Week2 | Variables</title>
-</head>
-<body>
-    <?php
-        $name  = "Srivignesh";
+<!doctype html>
+<html>
+  <head>
+    
+    <title>PHP and Creating Output</title>
+    
+  </head>
+  <body>
+  
+    <?php echo "<h1>PHP and Creating Output</h1>" ;
+
+    echo "<p>The PHP echo command can be used to create output.</p>";
+
+    echo "<p>When creating output using echo and PHP, quotes can often cause problems. There are several solutions to using quotes within an echo statement:</p>";
+
+    echo "    
+    <ul>
+        <li>Use HTML special characters</li>
+        <li>Alternate between single and double quotes</li>
+        <li>Use a backslash to escape quotes</li>
+    </ul>"
     ?>
-    Hi <?php echo $name?>!
-    <?php echo "Hey," .$name. "!"?>
-</body>
+    <h2>More HTML to Convert</h2>
+
+    <p>PHP says "Hello World!"</p>
+
+    <p>Can you display a sentence with ' and "?</p>
+
+    <img src="php-logo.png">
+
+    <?php 
+        echo '<img src="php-logo.png">';
+        
+    ?>
+
+    <img src="<?php echo 'php-logo.png'?>" alt="">
+
+    <?php 
+    $fName = "Srivignesh Kavle";
+    $lName = "Sathyanarayanan";
+    ?>
+    <p> My Full Name is: <?php echo $fName." ".$lName ?>
+    
+
+  </body>
 </html>
